0ea86bf4fedeb16d063952dc0e797ab436588894
Fernando Muzaber <fmuzaber@economicas.uba.ar>
2023-12-07 14:55:01 -0300

    updates package.json

--- server/frontend/src/App.tsx
+++
@@ -1,9 +0,0 @@
-function App() {
-  return (
-    <div className="flex items-center justify-center h-screen">
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
-    </div>
-  );
-}
-
-export default App;
