047dc5c72af6c58ce78572b6ad7a93b12bd28a11
Fernando Muzaber <fmuzaber@economicas.uba.ar>
2023-12-15 07:50:19 -0300

    updates

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const express = require("express");
 const cors = require("cors");
 const routes = require("./routes");
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8080;
 const app = express();
 const axios = require("axios");
 
